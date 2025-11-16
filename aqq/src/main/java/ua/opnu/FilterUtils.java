81cf91405402911dc2603b1ae6d8d0f20a3e9186
Anna Zadorozhna <9242104@stud.op.edu.ua>
2025-11-16 23:33:17 +0200

    lad_8 commit

---
+++ aqq/src/main/java/ua/opnu/FilterUtils.java
@@ -0,0 +1,23 @@
+package ua.opnu;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class FilterUtils {
+    public static <T> T[] filter(T[] input, Predicate<T> predicate) {
+        int count = 0;
+        for (T element : input) {
+            if (predicate.test(element)) count++;
+        }
+
+        T[] result = Arrays.copyOf(input, count);
+        int index = 0;
+        for (T element : input) {
+            if (predicate.test(element)) {
+                result[index++] = element;
+            }
+        }
+
+        return result;
+    }
+}
